d1c6cdf4492eaf398e27a225e7d7354ac4806483
dcordovao <diego.cordova.14@sansano.usm.cl>
2019-08-26 21:42:17 -0400

    Pregunta 3 lista sin nuevos reduce.

--- tarea4/clean.cu
+++ tarea4/clean.cu
@@ -40,9 +40,8 @@ __device__ float dist(float x1, float y1, float x2, float y2)
 
 
 
-__global__ void charge(float l, cell *map)
+__global__ void add_charges(float l, cell *map)
 {
-
   int i = blockIdx.x*blockDim.x + threadIdx.x;
   float rowParticle,colParticle,rowCell,colCell;
 
@@ -57,6 +56,24 @@ __global__ void charge(float l, cell *map)
       if (distancia != -1) {
         map[i].charge += distancia;
       }
+    }
+  }
+}
+
+__global__ void add_one_particle(int l, int part_pos, cell *map)
+{
+  int i = blockIdx.x*blockDim.x + threadIdx.x;
+  float rowParticle,colParticle,rowCell,colCell;
+
+  if (i<l)
+  {
+    rowParticle = (part_pos / WIDTH); 
+    colParticle = (part_pos % WIDTH);
+    rowCell = (i / WIDTH);
+    colCell = (i % WIDTH);
+    float distancia = (dist(rowParticle,colParticle,rowCell,colCell));
+    if (distancia != -1) {
+      map[i].charge += distancia;
     }
   }
 }
@@ -161,7 +178,7 @@ int main(int argc, char *argv[]){
   cudaEventRecord(ct1);
 
   // Charge grid
-  charge<<<gridSize,blockSize>>>(WIDTH*LENGHT, d_cells);
+  add_charges<<<gridSize,blockSize>>>(WIDTH*LENGHT, d_cells);
   cudaDeviceSynchronize();
 
   //Time after charge kernel
@@ -229,16 +246,91 @@ int main(int argc, char *argv[]){
 
   std::cout << "Time GPU computing minimum value: " << time2 << "[ms]" << std::endl;
 
-  for (size_t i = 0; i < (gridSize/blockSize)/blockSize; i++) {
+  cell mejor_cell = out[0];
+  for (size_t i = 0; i < large_out_3; i++) {
+    mejor_cell = (out[i].charge < mejor_cell.charge)? out[i]: mejor_cell;
     cout << "Charge: " << out[i].charge << "   Pos: " << out[i].index << endl;
+  }
+
+  cell *best_1000_cells;
+  best_1000_cells = (cell *)malloc(1000 * sizeof(cell));
+  cell *inf_array;
+  inf_array = (cell *)malloc(sizeof(cell));
+
+  // Primera mejor celda
+  best_1000_cells[0] = mejor_cell;
+  float reduction_total_time = 0, add_charges_total_time = 0;
+
+  for (int i = 1; i < 1000; i++)
+  {
+
+    float dt3;
+    // time before kernel charge
+    cudaEventCreate(&ct1);
+    cudaEventCreate(&ct2);
+    cudaEventRecord(ct1);
+
+    add_one_particle<<<gridSize,blockSize>>>(WIDTH*LENGHT, best_1000_cells[i-1].index, d_cells);
+    cudaDeviceSynchronize();
+    
+    //Time after charge kernel
+    cudaEventRecord(ct2);
+    cudaEventSynchronize(ct2);
+    cudaEventElapsedTime(&dt3, ct1, ct2);
+
+    add_charges_total_time += dt3;
+
+    mejor_cell.charge = inf;
+    inf_array[0] = mejor_cell;
+    CUDA_CHECK(cudaMemcpy(&d_cells[mejor_cell.index], inf_array, sizeof(cell), cudaMemcpyHostToDevice));
+
+    
+    // time before min charge
+    cudaEventCreate(&ct1);
+    cudaEventCreate(&ct2);
+    cudaEventRecord(ct1);
+
+    // Search min load
+    reduce0<<<large_out_1,blockSize,sharedBytes>>>(d_cells, dev_out);
+    cudaDeviceSynchronize();
+    reduce0<<<large_out_2, blockSize, sharedBytes>>>(dev_out, dev_out2);
+    cudaDeviceSynchronize();
+    reduce0<<<large_out_3,blockSize,sharedBytes>>>(dev_out2, dev_out3);
+    cudaDeviceSynchronize();
+
+    //Time after min kernel
+    cudaEventRecord(ct2);
+    cudaEventSynchronize(ct2);
+    cudaEventElapsedTime(&dt3, ct1, ct2);
+
+    reduction_total_time += dt3;
+
+    CUDA_CHECK(cudaMemcpy(out, dev_out3, ((gridSize/blockSize)/blockSize)*sizeof(cell), cudaMemcpyDeviceToHost));
+    
+
+    mejor_cell = out[0];
+    for (size_t j = 0; j < large_out_3; j++) {
+      mejor_cell = (out[j].charge < mejor_cell.charge)? out[j]: mejor_cell;
+    }
+
+    best_1000_cells[i] = mejor_cell;
   }
 
   // Escribiendo resultado en archivo
   ofstream times_file;
-  times_file.open("results_tarea_4_2.txt", ios_base::app);
+  times_file.open("results_tarea_4_2_final.txt");
   times_file << input_file_name.c_str() << endl;
-  times_file << "Tiempo en charge kernel: "<< dt << "[ms]" << endl;
-  times_file << "Tiempo en min kernel: "<< dt2 << "[ms]" << endl;
+  times_file << "Tiempo en charge kernel primera iteracion: "<< dt << "[ms]" << endl;
+  times_file << "Tiempo en min kernel primera iteracion: "<< dt2 << "[ms]" << endl;
+  times_file << "Tiempo en charge kernel 999 iteraciones: "<< add_charges_total_time << "[ms]" << endl;
+  times_file << "Tiempo en min kernel 999 iteraciones: "<< reduction_total_time << "[ms]" << endl;
+
+  times_file << "\n Resultados 1000 casillas (Carga, posicion): \n";
+
+  for (int i = 0; i < 1000; i++)
+  {
+    times_file << best_1000_cells[i].charge << "    " << best_1000_cells[i].index << endl;
+  }
 
   cudaFree(d_cells);
   cudaFree(dev_out);
